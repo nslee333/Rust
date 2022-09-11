876c5ba32c63338c82b4246806553d5aa51d5893
Nathan <nslee333@gmail.com>
2022-09-10 17:18:17 -0700

    Rust Chapter1.

---
+++ hello_cargo/target/debug/deps/hello_cargo-eb063a268d4c4829.d
@@ -0,0 +1,5 @@
+C:\Users\nslee\Programming\Rust\hello_cargo\target\debug\deps\hello_cargo-eb063a268d4c4829.rmeta: src\main.rs
+
+C:\Users\nslee\Programming\Rust\hello_cargo\target\debug\deps\hello_cargo-eb063a268d4c4829.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\nslee\Programming\Rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\nslee\Programming\Rust\hello_cargo\src\main.rs
