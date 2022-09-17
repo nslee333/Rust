85292bfc63d9cb95ad9379c62e65b4f6c5e720b5
Nathan <nslee333@gmail.com>
2022-09-16 19:36:08 -0700

    Rust book progress.

---
+++ hello_cargo/target/debug/deps/hello_cargo-312bd57cc3744674.d
@@ -0,0 +1,5 @@
+c:\Users\nslee\Programming\Rust\hello_cargo\target\debug\deps\hello_cargo-312bd57cc3744674.rmeta: src\main.rs
+
+c:\Users\nslee\Programming\Rust\hello_cargo\target\debug\deps\hello_cargo-312bd57cc3744674.d: src\main.rs
+
+src\main.rs:
