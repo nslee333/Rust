3f244a5a08030858ee679fffae575b4c10d176e0
Nathan <nslee333@gmail.com>
2022-09-18 11:07:17 -0700

    Finished fahrenheit.

--- fahrenheit/src/main.rs
+++ fahrenheit/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
    
-    fahrenheit_to_celsius(100.0);
-    celsius_to_fahrenheit(100.0);
+    println!("{}", fahrenheit_to_celsius(100.0));
+    println!("{}", celsius_to_fahrenheit(100.0));
 
 }
 
-fn fahrenheit_to_celsius(fahrenheit: f32) {
-    fahrenheit - 32 / 1.8
+fn fahrenheit_to_celsius(fahrenheit: f32) -> f32 {
+    (fahrenheit - 32.0) / 1.8
 }
 
-fn celsius_to_fahrenheit(celsius: f32) {
-    celsius * 1.8 + 32
+fn celsius_to_fahrenheit(celsius: f32) -> f32 {
+    (celsius * 1.8) + 32.0
 }
