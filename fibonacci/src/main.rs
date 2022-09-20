0e36746826fff49196f316e11a2d6064d2d5db6a
Nathan <nslee333@gmail.com>
2022-09-20 15:12:49 -0700

    Fibonacci.

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    
+    println!("{}", fibonacci(9));
 }
 
 fn fibonacci(number: i32) -> i32 {
-    
+    if number <= 1 {
+        return number;
+    }
+    return fibonacci(number - 1) + fibonacci(number - 2);
+
 }
