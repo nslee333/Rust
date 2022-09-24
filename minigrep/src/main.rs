ef3ca22a434ea1b8e586704a3bc0663f78ca4a69
Nathan <nslee333@gmail.com>
2022-09-23 18:38:26 -0700

    init minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("You have been able to read the file.");
+
+    println!("With text:\n{contents}");
+}
+ 
